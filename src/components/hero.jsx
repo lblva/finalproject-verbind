7763e547930a2897674b04e54382d9e8aba4b22a
lblva <lola.batchalova@hotmail.com>
2025-01-19 20:10:10 +0100

    initial commit with hero component

---
+++ src/components/hero.jsx
@@ -0,0 +1,32 @@
+import '../styles/hero.css'
+
+
+function Hero() {  
+
+  return (
+    <>
+      <div className="heroContainer">
+        <div className="heroLinks">
+          <div className='heroLinksInner'>
+            <p className='heroTitle'>Vind sneller werk met Verbind
+            de brug tussen ambitie en succes</p>
+            <div className='heroTextContainer'>
+              <p className='heroText'>Je zoektocht naar werk begint hier.<br/> Verbind helpt je sneller werk te vinden door je te verbinden met vacatures die perfect bij jouw passen.  Vind de perfecte match met slechts één swipe.</p>
+             </div>
+            <div className='getTheAppContainer'>
+             <div className='getTheApp'>
+              <p className='heroText'>Get the app &rarr;</p>
+             </div>
+             <img src='/qrCode.png' alt='qrCode' className='qrCode'/>
+            </div>
+          </div>
+        </div>
+        <div className="heroRechts">
+          <img src="/heroImage.png" alt="heroImage" className='heroImage'/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
